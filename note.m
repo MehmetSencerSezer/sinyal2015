67b421b985e2f4ad69ec5fc9c74c39a9d02a8716
MehmetSencerSezer <sencersezer@gmail.com>
2015-12-29 13:02:48 +0200

    note.m dosyasının güncellenmesi.

--- note.m
+++ note.m
@@ -1,16 +1,20 @@
-function [xx,tt] = note(ff,dur)
-tt1 = 0:1/(20*ff):dur*1/4*1.5; 
-xx1 =  1.5*sin(2*pi*ff*tt1);
 
-tt2 = dur*1/4:1/(20*ff):(dur*1/4)+(dur*1/8); 
-xx2 = sin(2*pi*ff*tt2);
- 
-tt3 =(dur*1/4)+(dur*1/8):1/(20*ff):(dur*1/4)+(dur*1/8)+(dur*1/2);
-xx3 = sin(2*pi*ff*tt3);
-
-tt4 = (dur*1/4)+(dur*1/8)+(dur*1/2):1/(20*ff):(dur*1/4)+(dur*1/8)+(dur*1/2)+(dur*1/8);
-xx4 = sin(2*pi*ff*tt4);
-
-tt = 0:1/(20*ff):dur;
-xx = xx1+xx2+xx3+xx4;
-plot(tt,xx)
+function [topla,t]=note(ff,olcu) 
+fs=8192; %örnekleme frekansı
+a=1;
+topla=0;
+harmonik={1,0.8,0.4,0.1}; %harmonik dizisinin tanımlanması.
+t=0:1/olcu:olcu-(1/fs); %t araliğinin hesaplanması.
+for h=1:4
+   x=harmonik{h}*sin(2*pi*ff*t*a); %Harmonik hesaplama
+   topla=topla+x;
+   a=a+1;
+end
+   z1=linspace(0,1.5,length(t)/4);
+   z2=linspace(1.5,1,length(t)/8);
+   z3=linspace(1,1,length(t)/2);
+   z4=linspace(1,0,length(t)/8);
+   zarf=[z1 z2 z3 z4];
+   x=x.*zarf;
+ echo=((x-(fs/10))*30/100)+x; %echo nun hesaplanması
+end

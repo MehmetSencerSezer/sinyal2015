ed1d7a91f2acf46569e43725752ab3062f487fd3
MehmetSencerSezer <sencersezer@gmail.com>
2015-12-29 13:12:48 +0200

    note.m dosyasının güncellenmesi.

--- note.m
+++ note.m
@@ -1,14 +1,13 @@
 
-function [topla,t]=note(ff,olcu) 
+function [xx,t]=note(ff,olcu) 
 fs=8192; %örnekleme frekansı
-a=1;
-topla=0;
+xx=0;
 harmonik={1,0.8,0.4,0.1}; %harmonik dizisinin tanımlanması.
 t=0:1/olcu:olcu-(1/fs); %t araliğinin hesaplanması.
 for h=1:4
-   x=harmonik{h}*sin(2*pi*ff*t*a); %Harmonik hesaplama
-   topla=topla+x;
-   a=a+1;
+   x=harmonik{h}*sin(2*pi*ff*t); %Harmonik hesaplama
+   xx=xx+x;
+
 end
    z1=linspace(0,1.5,length(t)/4);
    z2=linspace(1.5,1,length(t)/8);

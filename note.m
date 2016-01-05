480bca29cbd8db9fccb5a088aae40bc5e26e0d73
MehmetSencerSezer <sencersezer@gmail.com>
2016-01-05 11:17:14 +0200

    note.m dosyasının güncellenmesi.

--- note.m
+++ note.m
@@ -1,12 +1,12 @@
 
 function [xx,t]=note(ff,olcu) 
 fs=8192; %örnekleme frekansı
-xx=0;
+harmo_x=0;
 harmonik={1,0.8,0.4,0.1}; %harmonik dizisinin tanımlanması.
 t=0:1/olcu:olcu-(1/fs); %t araliğinin hesaplanması.
-for h=1:4
-   x=harmonik{h}*sin(2*pi*ff*t); %Harmonik hesaplama
-   xx=xx+x;
+for h=1:length(harmonik)%Her harmoniğin hesaplanması için kurduğum döngü
+   xx=harmonik{h}*sin(2*pi*ff*t); %Harmonik hesaplama
+   harmo_x=harmo_x+x;% Her değer için toplayıp üzerine ekleme
 
 end
    z1=linspace(0,1.5,length(t)/4);
@@ -14,6 +14,5 @@ end
    z3=linspace(1,1,length(t)/2);
    z4=linspace(1,0,length(t)/8);
    zarf=[z1 z2 z3 z4];
-   xx=xx.*zarf;
- echo=((xx-(fs/10))*30/100)+xx; %echo nun hesaplanması
+   xx=topla.*zarf; %Harmoniklendikten sonra sinyalin zarflanması
 end
